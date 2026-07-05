5fcd447e3179d15e48fdee695c463f80ffbed3a5
kyh33333 <kyh090410@gmail.com>
2026-07-06 01:13:50 +0900

    1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if(moveInput.sqrMagnitude > 0)
+        {
+            moveInput = moveInput.normalized;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
